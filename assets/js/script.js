b8fee28f43855723e4a45e5747b2eaa4e890fbb2
andersonjaz <91381863+andersonjaz@users.noreply.github.com>
2022-05-03 20:28:31 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var searchBarEl = document.getElementById("search-bar")
 
 var breweryDisplayEL = document.getElementById("breweries")
 var listItemEl = document.getElementById('list')
-var weatherEl = document.querySelector("weather")
+var weatherEl = document.getElementById("weather")
 const apiKey = "82d466c6476a45fdadcc1da99e41ba61"
 
 // varable to add unique id to each list item
@@ -44,53 +44,28 @@ function getBreweryName() {
 var getWeather = function(){
     var cityName = searchBarEl.value
     console.log(cityName)
-    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`
+    var apiURL = `https://metaweather.com/api/location/search/?q=${cityName}`
 
     fetch(apiURL)
     
-    .then(function(response){
-        response.json().then(function(data){
-            console.log(data)
-           display5Day(data);
-        });
-    });
-};
+    .then(function(response) {
+        return response.json();
+   })
 
-var display5Day = function(){
-    weatherEl.textContent = ""
-
-    var forecast = weather.list;
-        for(var i=5; i < forecast.length; i++){
-       var dailyForecast = forecast[i];
-        
-
-       console.log(dailyForecast)
-
-       //set date element
-       var forecastDate = document.createElement("h5")
-       forecastDate.textContent= moment.unix(dailyForecast.dt).format("MMM DD, YYYY");
-       forecastDate.classList = "card-header text-center"
-       forecastEl.appendChild(forecastDate);
-       
-       //set temperature span
-       var forecastTempEl=document.createElement("span");
-       forecastTempEl.classList = "card-body text-center";
-       forecastTempEl.textContent = dailyForecast.main.temp + " °F";
-
-        //append to forecast card
-        forecastEl.appendChild(forecastTempEl);
-
-        // console.log(forecastEl);
-       //append to five day container
-        weatherEl.appendChild(forecastEl);
-    }
-
+   .then(function(data) {
+     for(var i=0; i < data.length; i++){
+        var listItem = document.createElement('li');
+        listItem.id = 'list2' + listItemCounter++;
+     }
+       console.log(data[i]);
+});
 }
 
+
 function doBoth() {
-   // event.preventDefault()
+   //preventDefault()
     getBreweryName()
     getWeather()
 }
-   
+
 searchButton.addEventListener("click", doBoth);
