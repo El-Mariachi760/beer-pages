0a2d373c32a17bb4f6debff3549f2780e630aa19
Pancho Villa <94568874+El-Mariachi760@users.noreply.github.com>
2022-05-04 14:16:11 -0700

    Updated weather api link

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,7 +47,7 @@ function getBreweryName() {
 var getWeather = function(){
     var cityName = searchBarEl.value
     console.log(cityName)
-    var apiURL = `https://metaweather.com/api/location/search/?q=${cityName}`
+    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
 
     console.log(apiURL)
     fetch(apiURL)
